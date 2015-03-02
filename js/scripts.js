ac3f766e74a9ac0eea5caee43000088582997ceb
Travis Sisti <travhimself@gmail.com>
2015-03-01 21:55:16 -0700

    initial commit of new design with parallax scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 $('document').ready(function() {
 
+    // kick off skrollr for parallax effect on desktop
+    enquire.register("screen and (min-width: 1025px)", function() {
+        var skrl = skrollr.init();
+    });
 
+
+    // mask email address by writing it in on the fly
+    var user = "trav";
+    var domain = "travhimself.com";
+    $('nav a#email').attr('href', 'mail' + 'to:' + user + '@' + domain);
 
 });
